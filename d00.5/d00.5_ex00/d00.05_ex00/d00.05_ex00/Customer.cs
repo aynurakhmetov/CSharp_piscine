3f03ffde8e344d3d594bdc116b0011f16855982f
aynurakhmetov <aynur.helloworld@gmail.com>
2021-09-01 21:38:14 +0300

    ex00, ex01, ex02

--- d00.5/d00.5_ex00/d00.05_ex00/d00.05_ex00/Customer.cs
+++ d00.5/d00.5_ex00/d00.05_ex00/d00.05_ex00/Customer.cs
@@ -6,16 +6,18 @@ namespace ex01
     {
         private string Name{ get; set; }
         private int OrderNumOfClient { get; set; }
+        public int NumOfItemsInCart { get; private set; }
 
         public Customer(string n, int o)
         {
             this.Name = n;
-            this.OrderNumOfClient = 0;
+            this.OrderNumOfClient = o;
+            this.NumOfItemsInCart = 0;
         }
 
         public override string ToString()
         {
-            return "Name = " + this.Name + ", num = " + this.OrderNumOfClient;
+            return "Name = " + this.Name + ", Order number of customer = " + this.OrderNumOfClient;
         }
         
         public static bool operator==(Customer c1, Customer c2)
@@ -32,6 +34,12 @@ namespace ex01
                 return false;
             else
                 return true;
+        }
+
+        public void FillCart(int max)
+        {
+            Random rnd = new Random();
+            this.NumOfItemsInCart = rnd.Next(0, max);
         }
     }
 }
--- d00.5/d00.5_ex00/d00.05_ex00/d00.05_ex00/Program.cs
+++ d00.5/d00.5_ex00/d00.05_ex00/d00.05_ex00/Program.cs
@@ -2,6 +2,12 @@
 using ex00;
 using ex01;
 
-var customer1 = new Customer("Andrew", 1);
-var customer2 = new Customer("Andrew", 1);
-Console.WriteLine(customer1 == customer2);
+var customer1 = new Customer("Anya", 1);
+var customer2 = new Customer("Yan", 2);
+var customer3 = new Customer("Vlad", 3);
+customer1.FillCart(15);
+customer2.FillCart(15);
+customer3.FillCart(15);
+Console.WriteLine(customer1.ToString() + ", Number of items in his cart = " + customer1.NumOfItemsInCart);
+Console.WriteLine(customer2.ToString() + ", Number of items in his cart = " + customer2.NumOfItemsInCart);
+Console.WriteLine(customer3.ToString() + ", Number of items in his cart = " + customer3.NumOfItemsInCart);
