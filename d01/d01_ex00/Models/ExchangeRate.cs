f78ecee171c7c6fe9f4ba848f747aeb87e161751
aynurakhmetov <aynur.helloworld@gmail.com>
2021-09-04 14:24:42 +0300

    Program.cs updated

--- d01/d01_ex00/Models/ExchangeRate.cs
+++ d01/d01_ex00/Models/ExchangeRate.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace ex00
 {
     struct ExchangeRate
--- d01/d01_ex00/Program.cs
+++ d01/d01_ex00/Program.cs
@@ -3,60 +3,61 @@ using System.IO;
 using System.Collections.Generic;
 using ex00;
 
-static void ErrorMassage()
+static void ErrorMessage()
 {
     Console.WriteLine("Ошибка ввода. Проверьте входные данные и повторите запрос.");
 }
 
 if (args.Length != 2)
 {
-    ErrorMassage();
+    ErrorMessage();
     return;
 }
 
-ExchangeSum exSum = new ExchangeSum(args[0]);
-//Console.WriteLine("sum = {0}, identify = {1}", exSum.sum, exSum.currencyIdentifier);
+var exchangeSum = new ExchangeSum(args[0]);
+//Console.WriteLine("sum = {0}, identify = {1}", exchangeSum.sum, exchangeSum.currencyIdentifier);
 
-List<string> currencyID = new List<string>() {"USD", "RUB", "EUR"};
-List<ExchangeRate> exRate = new List<ExchangeRate>();
-for(int i = 0; i < 3; i++)
+var currencyIdList = new List<string>() {"USD", "RUB", "EUR"};
+var exchangeRateList = new List<ExchangeRate>();
+for(var i = 0; i < 3; i++)
 {
-    string path = args[1] + "/" + currencyID[i] + ".txt";
-    FileInfo fileInf = new FileInfo(path);
-    if (!fileInf.Exists)    
+    var path = args[1] + "/" + currencyIdList[i] + ".txt";
+    var fileInfo = new FileInfo(path);
+    if (!fileInfo.Exists)    
     {
-        ErrorMassage();
+        ErrorMessage();
         return;
     }
-    StreamReader f = new StreamReader(path);
-    while (!f.EndOfStream)
+    var streamReader = new StreamReader(path);
+    while (!streamReader.EndOfStream)
     {
-        string s = f.ReadLine();
-        exRate.Add(new ExchangeRate(currencyID[i], s.Split(":")[0]
-                                                            , s.Split(":")[1]));
+        var readLine = streamReader.ReadLine();
+        exchangeRateList.Add(new ExchangeRate(currencyIdList[i], 
+            readLine .Split(":")[0], readLine .Split(":")[1]));
     }
-    f.Close();
+    streamReader.Close();
 }
 
 // for (int i = 0; i < 6; i++)
-//     Console.WriteLine("cFrom = {0}, cTo = {1}, coef = {2}", exRate[i].currencyFrom, exRate[i].currencyTo, exRate[i].coefficient);
+//     Console.WriteLine("cFrom = {0}, cTo = {1}, coef = {2}",
+// exchangeRateList[i].currencyFrom, exchangeRateList[i].currencyTo, exchangeRateList[i].coefficient);
 
-Exchanger exchanger = new Exchanger(exSum, exRate);
-string sum = exSum.sum;
-int indexOfChar = sum.IndexOf('.');
+var exchanger = new Exchanger(exchangeSum, exchangeRateList);
+var sum = exchangeSum.sum;
+var indexOfChar = sum.IndexOf('.');
 if (indexOfChar == -1)
     sum = sum + ".00";
 else
     sum = sum.Substring(0, indexOfChar + 3);
 
-Console.WriteLine("Сумма в исходной валюте: {0} {1}", sum, exSum.currencyIdentifier);
+Console.WriteLine("Сумма в исходной валюте: {0} {1}", sum, exchangeSum.currencyIdentifier);
 
 for (int i = 0; i < 3; i++)
 {
-    if (currencyID[i] != exSum.currencyIdentifier)
+    if (currencyIdList[i] != exchangeSum.currencyIdentifier)
     {
         decimal result = -1;
-        result = exchanger.ToConvertCurrency(currencyID[i]);
-        Console.WriteLine("Сумма в {0}: {1:f2} {0}", currencyID[i], result);
+        result = exchanger.ToConvertCurrency(currencyIdList[i]);
+        Console.WriteLine("Сумма в {0}: {1:f2} {0}", currencyIdList[i], result);
     }
 }
