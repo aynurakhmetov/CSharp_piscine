209db0dc03605326b405a216a643d2b0404a2adf
aynurakhmetov <aynur.helloworld@gmail.com>
2021-09-22 18:19:24 +0300

    ex02 is ready

--- d02.1/Configuration/Sources/YamlSource.cs
+++ d02.1/Configuration/Sources/YamlSource.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
+using System.IO;
+using YamlDotNet.Serialization;
 
 namespace d02._1
 {
@@ -9,14 +10,26 @@ namespace d02._1
         public string Path { get; set; }
         public string Priority { get; set;}
         public Hashtable Params { get; set; }
+        public YamlSource(string path, string priotity)
+        {
+            this.Path = path;
+            this.Priority = priotity;
+        }
 
         public void Deserialize()
         {
-            
-        }
-        public YamlSource(string path, string priotity)
-        {
-            
+            try
+            {
+                string yamlFileString = File.ReadAllText(Path);
+                var deserializer = new DeserializerBuilder().Build();
+                Params = deserializer.Deserialize<Hashtable>(yamlFileString);
+                // foreach(DictionaryEntry param in Params)
+                //     Console.WriteLine("Key: {0}, Value: {1}", param.Key, param.Value);
+            }
+            catch
+            {
+                Console.WriteLine("Invalid data. Check your input and try again.");
+            }
         }
     }
 }
