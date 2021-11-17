d712184fd9e4ab755a90ef91962e42ff8647fc64
aynurakhmetov <aynur.helloworld@gmail.com>
2021-11-17 11:53:14 +0300

    Reading the configuration

---
+++ d03/d03.Host/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace d03.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            // Reading the configuration
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var configuration = builder.Build();
+            
+            string apiKey = configuration["ApiKey"];
+            Console.WriteLine(apiKey);
+            
+            
+        }
+    }
+}
