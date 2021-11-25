831cc4151566f628a048608b693d6dcefaeb8260
aynurakhmetov <aynur.helloworld@gmail.com>
2021-11-25 13:36:15 +0300

    ApodClientBase update

--- d03/d03.Nasa/ApiClientBase.cs
+++ d03/d03.Nasa/ApiClientBase.cs
@@ -24,26 +24,23 @@ namespace d03.Nasa.Lib
             HttpResponseMessage response = await client.GetAsync(url);
             response.EnsureSuccessStatusCode();
             var responseBody = await response.Content.ReadAsStringAsync();
-            if (response.StatusCode == HttpStatusCode.OK)
+            try
             {
-                Console.WriteLine(responseBody);
-                return JsonSerializer.Deserialize<T>(responseBody);
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    Console.WriteLine(responseBody);
+                    return JsonSerializer.Deserialize<T>(responseBody);
+                }
             }
-            else
+            catch(WebException e)
             {
-                Console.WriteLine(response.StatusCode);
-                return JsonSerializer.Deserialize<T>("what need");
+                Console.WriteLine(e.Status);
             }
-            // Реализацию HTTP GET запроса
-            // десериализацию ответа в нужный объект: HttpGetAsync<T>(string url)
-            // Все это логично сделать protected, потому что понадобится только наследникам.
-            // Обращаться к API мы будем с помощью HTTP GET запросов. 
-            // В REST это тип идемпотентных запросов, которые отвечают за получение данных и не изменяют их (для изменения пригодились бы, например, POST, PUT или DELETE запросы). 
-            // Для реализации HTTP-запросов вам понадобится HttpClient. 
-            // Если запрос был выполнен успешно и имеет StatusCode = 200 (OK), получаем из JSON необходимые данные и десериализуем их. 
-            // Если статус ответа иной, результат запроса необходимо вернуть тому, кто метод вызвал, и в нем вывести в консоль.
-            // Так как HTTP-запросы это I/O операция, для предотвращения блокировки программы на время выполнения запроса к удаленному ресурсу (серверу API), методы классов, содержащие эти запросы, должны быть асинхронными. 
-            // В этом вам помогут ключевые слова async/await. 
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return JsonSerializer.Deserialize<T>(response.StatusCode.ToString());
         }
     }
 }
