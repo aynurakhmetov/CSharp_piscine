f6819011788d8dc02ccf6c252bf767b23138c7a5
aynurakhmetov <aynur.helloworld@gmail.com>
2021-12-02 18:40:29 +0300

    Method DisplayMedia added in ApodClient class

--- d03/d03.Nasa/Apod/ApodClient.cs
+++ d03/d03.Nasa/Apod/ApodClient.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Dynamic;
 using System.Threading.Tasks;
 
@@ -6,21 +7,41 @@ namespace d03.Nasa.Lib
     public class ApodClient : ApiClientBase, INasaClient<int, Task<MediaOfToday[]>>
     {
         public int ResultCount { get; private set;}
+        public MediaOfToday[] media;
         private string _url = "https://api.nasa.gov/planetary/apod?api_key=";
-        private ApodClient(string apiKey) : base(apiKey)
+        public ApodClient() {}
+        public ApodClient(string apiKey) : base(apiKey)
         {
         }
         
-        public async Task<MediaOfToday[]> GetAsync(int input)
+        public async Task<MediaOfToday[]> GetAsync(int resultCount)
         {
-            ResultCount = input;
-            var media = new MediaOfToday[ResultCount];
+            ResultCount = resultCount;
+            this.media = new MediaOfToday[ResultCount];
             for (int i = 0; i < ResultCount; i++)
             {
                 media[i] = await this.HttpGetAsync<MediaOfToday>(this._url + this.apiKey);
             }
             //throw new System.NotImplementedException();
             return media;
+        }
+
+        public void DisplayMedia()
+        {
+            for (int i = 0; i < ResultCount; i++)
+            {
+                Console.WriteLine(media[i].Date);
+                Console.Write($"'{0}'", media[i].Title);
+                if (media[i].Copyright != "")
+                {
+                    Console.WriteLine(" by {0}", media[i].Copyright);
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+                Console.WriteLine($"{0}\n{1}", media[i].Explanation, media[i].Url);
+            }
         }
     }
 }
