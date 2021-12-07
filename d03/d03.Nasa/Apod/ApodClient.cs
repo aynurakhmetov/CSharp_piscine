d04a9bb9de7b69ac961f9e4bc2771e220890b71a
aynurakhmetov <aynur.helloworld@gmail.com>
2021-12-07 14:21:13 +0300

    Method GetAsync was changed

--- d03/d03.Nasa/Apod/ApodClient.cs
+++ d03/d03.Nasa/Apod/ApodClient.cs
@@ -1,62 +1,34 @@
 using System;
 using System.Dynamic;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace d03.Nasa.Lib
 {
     public class ApodClient : ApiClientBase, INasaClient<int, Task<MediaOfToday[]>>
     {
-        public int ResultCount { get; private set;}
         public MediaOfToday[] media;
-        private string _url = "https://api.nasa.gov/planetary/apod?api_key="; //?count=1
-        private string _count = "&count=";
-        private DateTime _endDate = System.DateTime.Today;
-        private DateTime _startDate;
+        private string _url = "https://api.nasa.gov/planetary/apod?count=";
+        private string _api = "&api_key=";
         public ApodClient() {}
-        public ApodClient(string apiKey) : base(apiKey)
-        {
-        }
-        
+        public ApodClient(string apiKey) : base(apiKey) {}
         public async Task<MediaOfToday[]> GetAsync(int resultCount)
         {
-            ResultCount = resultCount;
-            this.media = new MediaOfToday[ResultCount];
-
-            this._startDate = this._endDate.AddDays(-resultCount);
-
-            //media = await this.HttpGetAsync<MediaOfToday[]>(this._url + this.apiKey + this._count + resultCount);
-
-            for (int i = 0; i < ResultCount; i++)
-             { 
-                 //media[i] = await this.HttpGetAsync<MediaOfToday>(this._url + this.apiKey);
-                 
-                 
-                 this._startDate = this._endDate.AddDays(-1);
-                 this._url = "https://api.nasa.gov/planetary/apod?start_date=" + _startDate.ToString("yyyy-MM-dd")
-                             + "&end_date=" + _endDate.ToString("yyyy-MM-dd") + "&api_key=";
-                 media[i] = await this.HttpGetAsync<MediaOfToday>(this._url + this.apiKey);
-                 this._endDate = this._endDate.AddDays(-1);
-            }
-            //throw new System.NotImplementedException();
-            return media;
-        }
-
-        public void DisplayMedia()
-        {
-            for (int i = 0; i < ResultCount; i++)
+            this.media = new MediaOfToday[resultCount];
+            this._url = this._url + resultCount + this._api;
+            var statusCode = await this.GetStatusCodeAsync(this._url);
+            if (statusCode == HttpStatusCode.OK)
             {
-                Console.WriteLine(media[i].Date.ToString("d"));
-                Console.Write($"'{media[i].Title}'");
-                if (media[i].Copyright != "")
-                {
-                    Console.WriteLine($" by {media[i].Copyright}");
-                }
-                else
-                {
-                    Console.WriteLine();
-                }
-                Console.WriteLine($"{media[i].Explanation}\n{media[i].Url}\n");
+                media = await this.HttpGetAsync<MediaOfToday[]>(this._url);
             }
+            else
+            {
+                Console.WriteLine($"GET \"{this._url}\" returned {statusCode.ToString()}:");
+                var responseBody = await this.response.Content.ReadAsStringAsync();
+                Console.WriteLine($"{responseBody}");
+            }
+            return null;
         }
     }
 }
