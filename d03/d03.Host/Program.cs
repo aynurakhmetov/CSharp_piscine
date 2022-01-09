0baa36f841625da6dc0ab19580d06cfddbb006ca
aynurakhmetov <aynur.helloworld@gmail.com>
2022-01-10 01:26:21 +0300

    Program updated

--- d03/d03.Host/Program.cs
+++ d03/d03.Host/Program.cs
@@ -26,10 +26,22 @@ namespace d03.Host
                 }
                 else
                 {
-                    Console.WriteLine();
+                    //Console.WriteLine();
                 }
                 Console.WriteLine($"{media[i].Explanation}");
                 Console.WriteLine($"{media[i].Url}");
+                Console.WriteLine();
+            }
+        }
+
+        static void DisplayAsteroid(AsteroidLookup[] asteroid)
+        {
+            for (int i = 0; i < asteroid.Length; i++)
+            {
+                Console.WriteLine($"- Asteroid {asteroid[i].Name}, SPK-ID: {asteroid[i].Id}");
+                Console.Write($"IS POTENTIALLY HAZARDOUS!");
+                Console.WriteLine($"Classification: {asteroid[i].OrbitalData.OrbitClass.OrbitClassType}, {asteroid[i].OrbitalData.OrbitClass.OrbitClassDescription}.");
+                Console.WriteLine($"Url: {asteroid[i].NasaUrl}.");
                 Console.WriteLine();
             }
         }
@@ -43,19 +55,32 @@ namespace d03.Host
                 .AddJsonFile(configFile);
             var configuration = builder.Build();
             var apiKey = configuration["ApiKey"];
-            //Console.WriteLine(apiKey);
+            //Console.WriteLine($"ApiKey = {apiKey}");
 
+            var neoWs = new AsteroidRequest();
+            neoWs.StartDate = configuration["NeoWs:StartDate"];
+            neoWs.EndDate = configuration["NeoWs:EndDate"];
+            //Console.WriteLine($"NeoWs StartDate = {neoWs.StartDate}, EndDate = {neoWs.EndDate}");
+            
             // Get command from command line as arguments
             int resultCount;
+            //apiKey = "";
             if (args.Length == 2 && args[0] == "apod" && int.TryParse(args[1], out resultCount) && resultCount > 0)
             {
-                //apiKey = "";
                 var apodClient = new ApodClient(apiKey);
                 var mediaOfToday = await apodClient.GetAsync(resultCount);
                 //Console.WriteLine($"AAA {mediaOfToday[0].Title}");
                 if (mediaOfToday != null)
                     DisplayMedia(mediaOfToday);
                 // вывод данных здесь надо реализовать
+            }
+            else if (args.Length == 2 && args[0] == "neows" && int.TryParse(args[1], out resultCount) && resultCount > 0)
+            {
+                neoWs.ResultCount = resultCount;
+                var neoWsClient = new NeoWsClient(apiKey);
+                var asteroid = await neoWsClient.GetAsync(neoWs);
+                //if (asteroid != null)
+                //    DisplayAsteroid(asteroid);
             }
             else
             {
