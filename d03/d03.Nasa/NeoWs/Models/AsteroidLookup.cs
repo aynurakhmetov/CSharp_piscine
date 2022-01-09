ea55d12c73d838105863e8ae36d1af15ef541ca1
aynurakhmetov <aynur.helloworld@gmail.com>
2022-01-10 01:25:07 +0300

    AsteroidLookup is ready

--- d03/d03.Nasa/NeoWs/Models/AsteroidLookup.cs
+++ d03/d03.Nasa/NeoWs/Models/AsteroidLookup.cs
@@ -2,12 +2,36 @@ using System;
 using System.Dynamic;
 using System.Net;
 using System.Net.Http;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace d03.Nasa.Lib
 {
-    class AsteroidLookup
+    public class OrbitClass
     {
-        
+        [JsonPropertyName("orbit_class_type")]
+        public string OrbitClassType { get; set; }
+        [JsonPropertyName("orbit_class_description")]
+        public string OrbitClassDescription { get; set; }
+    }
+
+    public class OrbitalData
+    {
+        [JsonPropertyName("orbit_class")]
+        public OrbitClass OrbitClass { get; set; }
+    }
+    
+    public class AsteroidLookup
+    {
+        [JsonPropertyName("neo_reference_id")]
+        public string Id { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("nasa_jpl_url")]
+        public string NasaUrl { get; set; }
+        [JsonPropertyName("is_potentially_hazardous_asteroid")]
+        public bool IsPotentiallyHazardousAsteroid { get; set; }
+        [JsonPropertyName("orbital_data")]
+        public OrbitalData OrbitalData { get; set; }
     }
 }
