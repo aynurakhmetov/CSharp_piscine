8e7595c9682c819a7b990ea70273d23d0c46b66e
aynurakhmetov <aynur.helloworld@gmail.com>
2022-01-10 01:25:58 +0300

    NeoWsClient in work

--- d03/d03.Nasa/NeoWs/NeoWsClient.cs
+++ d03/d03.Nasa/NeoWs/NeoWsClient.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Dynamic;
 using System.Net;
 using System.Net.Http;
@@ -8,30 +9,65 @@ namespace d03.Nasa.Lib
 {
     public class NeoWsClient: ApiClientBase, INasaClient<AsteroidRequest, Task<AsteroidLookup[]>>
     {
-        // public MediaOfToday[] media;
-        // private string _url = "https://api.nasa.gov/planetary/apod?count=";
-        // private string _api = "&api_key=";
-        // public ApodClient() {}
-        // public ApodClient(string apiKey) : base(apiKey) {}
-        // public async Task<MediaOfToday[]> GetAsync(int resultCount)
-        // {
-        //     this.media = new MediaOfToday[resultCount];
-        //     this._url = this._url + resultCount + this._api;
-        //     var statusCode = await this.GetStatusCodeAsync(this._url);
-        //     if (statusCode == HttpStatusCode.OK)
-        //     {
-        //         media = await this.HttpGetAsync<MediaOfToday[]>(this._url);
-        //         return media;
-        //     }
-        //     else
-        //     {
-        //         Console.WriteLine($"GET \"{this._url}\" returned {statusCode.ToString()}:");
-        //         var responseBody = await this.response.Content.ReadAsStringAsync();
-        //         Console.WriteLine($"{responseBody}");
-        //         return null;
-        //     }
-        //}
+        public AsteroidLookup[] asteroidLookup;
+        private AsteroidRequest _asteroidRequest;
+        private string _url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=";
+        private string _urlNeoLookup = "https://api.nasa.gov/neo/rest/v1/neo/";
+        private string _endDate = "&end_date=";
+        private string _api = "&api_key=";
+        public NeoWsClient() {}
+        public NeoWsClient(string apiKey) : base(apiKey) {}
+
+        private async Task<List<int>> GetAsteroidId(int resultCount)
+        {
+            Console.WriteLine("\nI AM HERE 0\n");
+            Dictionary<String, AsteroidInfo> asteroidInfo;
+            
+            var statusCode = await this.GetStatusCodeAsync(this._url);
+            if (statusCode == HttpStatusCode.OK)
+            {
+                Console.WriteLine("\nI AM HERE 01\n");
+                asteroidInfo = await this.HttpGetAsync<Dictionary <String, AsteroidInfo> >(this._url);
+                Console.WriteLine($"\nI AM HERE 02 {asteroidInfo.Count}\n");
+                var id = new List<int>();
+                // int i = 0;
+                // while (i < resultCount && i < asteroidInfo.NearEarthObjects.AsteroidInfoList.Count)
+                // {
+                //     id.Add(asteroidInfo.NearEarthObjects.AsteroidInfoList[i].Id);
+                //     i++;
+                // }
+                return id;
+            }
+            else
+            {
+                Console.WriteLine($"GET \"{this._url}\" returned {statusCode.ToString()}:");
+                var responseBody = await this.response.Content.ReadAsStringAsync();
+                Console.WriteLine($"{responseBody}");
+                return null;
+            }
+        }
+        public async Task<AsteroidLookup[]> GetAsync(AsteroidRequest asteroidRequest)
+        {
+            this._asteroidRequest = asteroidRequest;
+            this._url = this._url + asteroidRequest.StartDate + this._endDate + asteroidRequest.EndDate + this._api;
+            Console.WriteLine($"\nresCount = {asteroidRequest.ResultCount}\n");
+            var id = GetAsteroidId(asteroidRequest.ResultCount);
+            this.asteroidLookup = new AsteroidLookup[id.Result.Count];
+
+            var newUrl = this._urlNeoLookup + id.Result[0] + "?api_key=";
+            var statusCode = await this.GetStatusCodeAsync(newUrl);
+            if (statusCode == HttpStatusCode.OK)
+            {
+                asteroidLookup = await this.HttpGetAsync<AsteroidLookup[]>(newUrl);
+                return asteroidLookup;
+            }
+            else
+            {
+                Console.WriteLine($"GET \"{this._url}\" returned {statusCode.ToString()}:");
+                var responseBody = await this.response.Content.ReadAsStringAsync();
+                Console.WriteLine($"{responseBody}");
+                return null;
+            }
+        }
     }
 }
-
-// Свойства и поля - лучшая практика разобраться
