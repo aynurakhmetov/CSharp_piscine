0588ce1f8cecb857b0537dfea4192f82eefce48e
aynurakhmetov <aynur.helloworld@gmail.com>
2022-01-10 01:25:25 +0300

    AsteroidRequest is ready

--- d03/d03.Nasa/NeoWs/Models/AsteroidRequest.cs
+++ d03/d03.Nasa/NeoWs/Models/AsteroidRequest.cs
@@ -2,14 +2,17 @@ using System;
 using System.Dynamic;
 using System.Net;
 using System.Net.Http;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace d03.Nasa.Lib
 {
     public class AsteroidRequest
     {
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+        [JsonPropertyName("StartDate")]
+        public string StartDate { get; set; }
+        [JsonPropertyName("EndDate")]
+        public string EndDate { get; set; }
         public int ResultCount { get; set; }
     }
 }
