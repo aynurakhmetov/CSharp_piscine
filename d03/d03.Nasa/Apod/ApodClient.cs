94ce0b281dd05ea8cfc2fde5f675c69c7b7c44e8
aynurakhmetov <aynur.helloworld@gmail.com>
2022-01-12 00:05:22 +0300

    is ready

--- d03/d03.Nasa/Apod/ApodClient.cs
+++ d03/d03.Nasa/Apod/ApodClient.cs
@@ -2,34 +2,37 @@ using System;
 using System.Net;
 using System.Threading.Tasks;
 
-namespace d03.Nasa.Lib
+namespace d03.Nasa
 {
     public class ApodClient : ApiClientBase, INasaClient<int, Task<MediaOfToday[]>>
     {
-        public MediaOfToday[] media;
+        private MediaOfToday[] _media;
         private string _url = "https://api.nasa.gov/planetary/apod?count=";
-        private string _api = "&api_key=";
-        public ApodClient() {}
+        private string _apiParameter = "&api_key=";
+        
+        private ApodClient() {}
+        
         public ApodClient(string apiKey) : base(apiKey) {}
+        
         public async Task<MediaOfToday[]> GetAsync(int resultCount)
         {
-            this.media = new MediaOfToday[resultCount];
-            this._url = this._url + resultCount + this._api;
+            this._media = new MediaOfToday[resultCount];
+            this._url = this._url + resultCount + this._apiParameter;
             var statusCode = await this.GetStatusCodeAsync(this._url);
+            
             if (statusCode == HttpStatusCode.OK)
             {
-                media = await this.HttpGetAsync<MediaOfToday[]>(this._url);
-                return media;
+                _media = await this.HttpGetAsync<MediaOfToday[]>(this._url);
+                return _media;
             }
             else
             {
                 Console.WriteLine($"GET \"{this._url}\" returned {statusCode.ToString()}:");
-                var responseBody = await this.response.Content.ReadAsStringAsync();
+                var response = await GetResponseAsync(this._url);
+                var responseBody = await response.Content.ReadAsStringAsync();
                 Console.WriteLine($"{responseBody}");
                 return null;
             }
         }
     }
 }
-
-// Свойства и поля - лучшая практика разобраться
