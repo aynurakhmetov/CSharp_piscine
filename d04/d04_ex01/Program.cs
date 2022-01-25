0f6c507d2acf0eb2c787ef2fb809f56fda3481c4
aynurakhmetov <aynur.helloworld@gmail.com>
2022-01-26 01:54:18 +0300

    d04_ex01 is ready

---
+++ d04/d04_ex01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.AspNetCore.Http;
+
+namespace d04_ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var defaultHttpContext = new DefaultHttpContext();
+            Console.WriteLine($"Old Response value: {defaultHttpContext.Response.ToString()}");
+            
+            var typeDefaultHttpContext = typeof(DefaultHttpContext);
+            var responseFieldInfo = typeDefaultHttpContext.GetField("_response",
+                BindingFlags.NonPublic | BindingFlags.Instance);
+            //Console.WriteLine($"Old Response value: {myFieldInfo.GetValue(defaultHttpContext)}");
+
+            responseFieldInfo.SetValue(defaultHttpContext, null);
+            Console.WriteLine($"New Response value: {responseFieldInfo.GetValue(defaultHttpContext)}");
+        }
+    }
+}
