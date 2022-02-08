53924ce77338ad3b65a5b9005c2a871c0cba50f8
aynurakhmetov <aynur.helloworld@gmail.com>
2022-02-08 23:20:07 +0300

    d04_ex02 is ready

--- d04/d04_ex02/ConsoleSetter/ConsoleSetter.cs
+++ d04/d04_ex02/ConsoleSetter/ConsoleSetter.cs
@@ -13,47 +13,65 @@ namespace d04_ex02
         
         public void SetValues<T>(T input) where T: class
         {
-            bool isNoDisplayAttribute = false;
-            
             var typeName = typeof(T);
             Console.WriteLine($"Let's set {typeName.Name}!");
             
+            //bool isNoDisplayAttribute;
+            
             foreach (var property in typeName.GetProperties(BindingFlags.Public | BindingFlags.Instance))
             {
-                System.Attribute[] attrs = System.Attribute.GetCustomAttributes(property);
-                foreach (System.Attribute attr in attrs)
+                //isNoDisplayAttribute = this.CheckNoDisplayAttributeOnProperty(property);
+                if (this.CheckNoDisplayAttributeOnProperty(property) == false)
                 {
-                    if (attr is NoDisplayAttribute)
-                    {
-                        isNoDisplayAttribute = true;
-                    }
+                    this.SetProperty(property, input);
                 }
-                if (isNoDisplayAttribute == false)
-                {
-                    AttributeCollection attributes = TypeDescriptor.GetProperties(input)[property.Name].Attributes;
-                    DescriptionAttribute myAttribute = (DescriptionAttribute)attributes[typeof(DescriptionAttribute)];
-                    if (myAttribute.Description != "")
-                        Console.WriteLine($"Set {myAttribute.Description}:");
-                    else
-                    {
-                        Console.WriteLine($"Set {property.Name}:");
-                    }
-                    var value = Console.ReadLine();
-                    if (value == "")
-                    {
-                        DefaultValueAttribute myAttribute2 = (DefaultValueAttribute)attributes[typeof(DefaultValueAttribute)];
-                        property.SetValue(input, myAttribute2.Value);
-                    }
-                    else
-                    {
-                        property.SetValue(input, value);
-                    }
-                }
-                isNoDisplayAttribute = false;
+                //isNoDisplayAttribute = false;
             }
+            
             Console.WriteLine();
             Console.WriteLine("We've set our instance!");
             Console.WriteLine($"{input.ToString()}");
+        }
+        
+        private bool CheckNoDisplayAttributeOnProperty(PropertyInfo property)
+        {
+            Attribute[] attributes = Attribute.GetCustomAttributes(property);
+            
+            foreach (var attribute in attributes)
+            {
+                if (attribute is NoDisplayAttribute)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void SetProperty<T>(PropertyInfo property, T input) where T: class
+        {
+            AttributeCollection attributes = TypeDescriptor.GetProperties(input)[property.Name].Attributes;
+            DescriptionAttribute descriptionAttribute = (DescriptionAttribute)attributes[typeof(DescriptionAttribute)];
+
+            if (descriptionAttribute.Description != "")
+            {
+                Console.WriteLine($"Set {descriptionAttribute.Description}:");
+            }
+            else
+            {
+                Console.WriteLine($"Set {property.Name}:");
+            }
+            
+            var value = Console.ReadLine();
+            
+            if (value == "")
+            {
+                DefaultValueAttribute defaultAttribute = (DefaultValueAttribute)attributes[typeof(DefaultValueAttribute)];
+                property.SetValue(input, defaultAttribute.Value);
+            }
+            else
+            {
+                property.SetValue(input, value);
+            }
         }
     }
 }
